b492ae941c5260aa209dc3ba5922c141dfb898e6
himanshukk81 <himanshukk81@gmail.com>
2021-03-20 18:40:21 +0530

    my work

--- src/navigation/stack.js
+++ src/navigation/stack.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, {useState} from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
 import Home from '../screens/Dashboard/Home';
 import Detail from '../screens/Dashboard/Detail';
 import LoginScreen from '../screens/Auth/Login';
 import OtpScreen from '../screens/Auth/Otp';
 import SignUpScreen from '../screens/Auth/SignUp';
 import LandingScreen from '../screens/Auth/Landing';
-import { useSelector, useDispatch } from "react-redux";
+import {useSelector, useDispatch} from 'react-redux';
 
 import i18n from 'i18n-js';
 import en from '../translations/en.json';
@@ -16,11 +16,11 @@ import ur from '../translations/ur.json';
 const Stack = createStackNavigator();
 
 const StackNavigator = () => {
-  const isLogin = useSelector(state => state.isLogin);
+  const isLogin = useSelector((state) => state.isLogin);
   const [language, setLanguage] = useState('en');
   i18n.locale = language;
   i18n.fallbacks = true;
-  i18n.translations = { en, hn, ur };
+  i18n.translations = {en, hn, ur};
 
   return (
     <Stack.Navigator
@@ -34,13 +34,12 @@ const StackNavigator = () => {
         </>
       ) : (
         <>
-          {/* <Stack.Screen name="Landing" component={LandingScreen} /> */}
-          <Stack.Screen name="SignIn" component={LoginScreen} />
+          <Stack.Screen name="Landing" component={LandingScreen} />
+          {/* <Stack.Screen name="SignIn" component={LoginScreen} /> */}
           <Stack.Screen name="Otp" component={OtpScreen} />
           <Stack.Screen name="SignUp" component={SignUpScreen} />
         </>
       )}
-
     </Stack.Navigator>
   );
 };
