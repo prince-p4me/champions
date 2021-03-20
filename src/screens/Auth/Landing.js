c11d83a42b2f2f76925b3f442823ccc3b3a6eb03
prince <prince.p4me@gmail.com>
2021-03-20 17:33:18 +0530

    landing screen

--- src/screens/Auth/Landing.js
+++ src/screens/Auth/Landing.js
@@ -1,46 +1,61 @@
 import React from 'react';
-import { View, Text, ImageBackground, Image, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, ImageBackground, Image, TouchableOpacity } from 'react-native';
 import Header from '../../components/Header';
-import styles from '../../utility/Style';
+import globalStyles from '../../utility/Style';
 import Images from '../../utility/Image';
 import Constant from '../../utility/Constant';
 import * as Navigation from '../../navigation/navigation';
 import Color from '../../utility/Color';
-import { TextRegular } from '../../components/TextView';
+import Sizes from '../../utility/Sizes';
+import { TextBold, TextRegular } from '../../components/TextView';
 import i18n from 'i18n-js';
 
 const LandingScreen = ({ route }) => {
 
     return (
         <ImageBackground source={Images.bgGreen} resizeMode="cover"
-            style={styles.container}>
-            <View style={{
-                width: Constant.width,
-                flex: 6,
-                alignItems: "center",
-                justifyContent: "space-between",
-                paddingHorizontal: "18%"
-            }}>
+            style={globalStyles.container}>
+            <View style={styles.firstSection}>
                 <Image source={Images.champLogo}
-                    style={{ width: "100%" }} resizeMode="contain"></Image>
-                <View style={{ flex: 6, width: "100%", backgroundColor: "red" }}>
-                    <TouchableOpacity style={{
-                        width: "100%",
-                        backgroundColor: Color.white,
-                        borderRadius: 25,
-                        justifyContent: "center",
-                        alignItems: "center",
-                        height: 45
-                    }}>
-                        <TextRegular text={i18n.t('login')} />
+                    style={{ width: "100%", height: "30%" }} resizeMode="contain"></Image>
+                <View style={styles.btnContainer}>
+                    <TouchableOpacity style={styles.button} activeOpacity={.7}
+                        onPress={() => alert("Hello")}>
+                        <TextBold text={i18n.t('login')}
+                            style={{ color: Color.theme, fontSize: Sizes.medium }} />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[styles.button, { backgroundColor: Color.semiGold }]} activeOpacity={.7}>
+                        <TextRegular text={i18n.t('signup')}
+                            style={{ color: Color.white, fontSize: Sizes.medium }} />
                     </TouchableOpacity>
                 </View>
             </View>
             <Image source={Images.saina}
-                style={{
-                    flex: 4
-                }} resizeMode="contain"></Image>
+                style={{ flex: 5 }} resizeMode="contain"></Image>
         </ImageBackground>
     );
 };
 export default LandingScreen;
+
+const styles = StyleSheet.create({
+    firstSection: {
+        width: Constant.width,
+        flex: 5,
+        alignItems: "center",
+        justifyContent: "space-between",
+        paddingHorizontal: "18%"
+    },
+    btnContainer: {
+        height: "50%", width: "100%",
+        paddingVertical: "5%",
+        justifyContent: "space-between"
+    },
+    button: {
+        width: "100%",
+        backgroundColor: Color.white,
+        borderRadius: 25,
+        justifyContent: "center",
+        alignItems: "center",
+        height: 45
+    }
+})
--- src/utility/Sizes.js
+++ src/utility/Sizes.js
@@ -1,10 +1,11 @@
 export default Sizes = {
   small: 10,
   regular: 12,
-  semiLarge: 14,
-  large: 16,
-  extraLarge: 18,
+  medium: 14,
+  semiLarge: 16,
+  large: 18,
+  extraLarge: 20,
   extraSmall: 8,
-  double: 20,
-  extraDouble: 22
+  double: 22,
+  extraDouble: 24
 };
