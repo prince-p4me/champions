d32182071ae12760f13ace4ac28e1e17685cec3f
prince <prince.p4me@gmail.com>
2021-03-22 00:59:19 +0530

    translation fixed

--- src/screens/Auth/Landing.js
+++ src/screens/Auth/Landing.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useReducer } from 'react';
 import {
   View,
   StyleSheet,
@@ -11,37 +11,48 @@ import Images from '../../utility/Image';
 import Constant from '../../utility/Constant';
 import * as Navigation from '../../navigation/navigation';
 import Color from '../../utility/Color';
-import i18n from '../../services/i18n';
+import I18n from '../../services/i18n';
 import FullButton from '../../components/FullButton';
+import { useSelector, useDispatch } from 'react-redux';
 
-const LandingScreen = ({route}) => {
+const LandingScreen = props => {
+  let language = useSelector((state) => state.getLanguage);
+  const forceUpdate = useReducer((bool) => !bool)[1];
+
+  useEffect(() => {
+    setTimeout(() => {
+      I18n.locale = language;
+      forceUpdate();
+    }, 10);
+  }, [language]);
+
   return (
     <ImageBackground
       source={Images.bgGreen}
       resizeMode="cover"
       style={globalStyles.container}>
-      <SafeAreaView style={{backgroundColor: Colors.theme}}></SafeAreaView>
+      <SafeAreaView style={{ backgroundColor: Colors.theme }}></SafeAreaView>
 
       <View style={styles.firstSection}>
         <Image
           source={Images.champLogo}
-          style={{width: '100%', height: '30%'}}
+          style={{ width: '100%', height: '30%' }}
           resizeMode="contain"></Image>
         <View style={styles.btnContainer}>
           <FullButton
             onPress={() => Navigation.navigate('SignIn')}
-            text={i18n.t('login')}></FullButton>
+            text={I18n.t('login')}></FullButton>
 
           <FullButton
             onPress={() => Navigation.navigate('SignUp')}
-            text={i18n.t('signup')}
+            text={I18n.t('signup')}
             textColor={Color.white}
             bgColor={Color.semiGold}></FullButton>
         </View>
       </View>
       <Image
         source={Images.saina}
-        style={{flex: 5}}
+        style={{ flex: 5 }}
         resizeMode="contain"></Image>
     </ImageBackground>
   );
